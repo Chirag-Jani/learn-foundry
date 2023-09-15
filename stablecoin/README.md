cdc0cb7414c0142393583726d973f2884337cbab
Chirag-Jani <chirag.jani@uniqualitech.com>
2023-09-15 23:55:42 +0530

    feat: started stablecoin

--- stablecoin/README.md
+++ stablecoin/README.md
@@ -1,66 +1,11 @@
-## Foundry
+## What we gon build??
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
-
-Foundry consists of:
-
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
-
-## Documentation
-
-https://book.getfoundry.sh/
-
-## Usage
-
-### Build
-
-```shell
-$ forge build
-```
-
-### Test
-
-```shell
-$ forge test
-```
-
-### Format
-
-```shell
-$ forge fmt
-```
-
-### Gas Snapshots
-
-```shell
-$ forge snapshot
-```
-
-### Anvil
-
-```shell
-$ anvil
-```
-
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
-```
+1. Relative Stability : Anchored or Pegged to USD ($1.00) (Floating ones are better though)
+   1. How can we do it??
+      1. Using Chainlink Pricefeed
+      2. Get the pricefeed and change the ETH & BTC for the equivalent $$ value or price
+2. Stability Mechanism (Minting) : Algorithmic (Decentralized)
+   1. People can only mint stable coin with enough collateral (coded)
+3. Collateral : Exogenous (Crypto) 
+   1. ETH (Wrapped)
+   2. BTC (Wrapped)
---
+++ stablecoin/src/DecentralizedStableCoin.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC20Burnable, ERC20} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Chirag Jani
+ * @notice None
+ * Collateral : Exogenous (wETH & wBTC)
+ * Minting : Algorithmic (Decentralized)
+ * Relative Stability : Pegged to USD
+ *
+ * This contract is meant to be governed by DSCEngine.
+ * This contract is just the ERC20 implementation of our StableCoin System
+ */
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin_MustBeMoreThanZero();
+    error DecentralizedStableCoin_BurnAmountExceedsBalance();
+    error DecentralizedStableCoin_AddressNotAllowed();
+
+    constructor() ERC20("Jani's Decentralized StableCoin", "JDSC") {}
+
+    function burn(uint _amount) public override onlyOwner {
+        uint bal = balanceOf(msg.sender);
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+
+        if (_amount > bal) {
+            revert DecentralizedStableCoin_BurnAmountExceedsBalance();
+        }
+
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin_AddressNotAllowed();
+        }
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+
+        _mint(_to, _amount);
+
+        return true;
+    }
+}
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations (Using SafeMath for Uint)
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
